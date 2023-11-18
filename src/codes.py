3cddb21082cdc7b85b0682ce12f3e2dfda51a532
DomenicoGaudioso711 <domenicogaudioso@outlook.it>
2023-11-19 00:22:13 +0100

    up

--- src/codes.py
+++ src/codes.py
@@ -2,6 +2,10 @@ from sectionproperties.analysis import Section
 from sectionproperties.pre import CompoundGeometry, Geometry, Material
 import os
 import matplotlib.pyplot as plt
+from pathlib import Path
+import streamlit as st
+import pandas as pd
+import numpy as np
 
 def elenca_files_cartella(path_cartella,  typeFile = None):
     elenco_files = []
@@ -18,18 +22,21 @@ def elenca_files_cartella(path_cartella,  typeFile = None):
 
     return elenco_files
 
-def importSection(dxfFile):
+def importSection(dxfFile, rotate = 0):
     
-    listGeom = []
+    dictGeom = {}
     for idxf in dxfFile:
-        geom = Geometry.from_dxf(idxf)
-        geom.create_mesh(mesh_sizes=[100])
-        listGeom.append(geom)
-        
-        #Section(geometry=geom).plot_mesh(materials=False)
-    return listGeom
+        path = Path(idxf)
+        geom = Geometry.from_dxf(path)
+        geom = Geometry.rotate_section(geom, rotate, "center")
+        geom.create_mesh(mesh_sizes=[1000])
 
-def assMat(listGeom, name = "Steel", E = 210e3, v=0.3, rho=7.85e-6, fy=250):
+        dictGeom[path.name.replace(".dxf", '')] = geom
+        
+        
+    return dictGeom
+
+def assMat(dictGeom, name = "Steel", E = 210e3, v=0.3, rho=7.85e-6, fy=250):
     
     material = Material(
     name=name,
@@ -40,33 +47,86 @@ def assMat(listGeom, name = "Steel", E = 210e3, v=0.3, rho=7.85e-6, fy=250):
     color="grey",
     )
     
-    for ilist in listGeom:
-        ilist.material = material
+    for ilist in dictGeom:
+        dictGeom[ilist].material = material
     
-    return listGeom
+    return dictGeom
 
-def calcPro(listGeom):
+def calcPro(dictGeom):
     dictionaryProp = {} #ASSEGNARE UNA DICTIONARY 
     
-    for ilist in listGeom:
-        section = Section(geometry=ilist)
+    for ilist in dictGeom:
+        section = Section(geometry=dictGeom[ilist])
         section.calculate_geometric_properties()
         section.calculate_warping_properties()
+        dictGeom[ilist] = section
         #section.calculate_plastic_properties()
         
         area = section.get_area()
+        igx, igy = section.get_c()
         ixx_c, iyy_c, ixy_c = section.get_ic()
+        wxx_pos, wxx_neg, wyy_pos, wyy_neg = section.get_z()
         phi = section.get_phi()
         j = section.get_j()
+        scx_c, scy_c = section.get_sc()
+        ax_c, ay_c = section.get_as() #area di taglio
         
-        print(f"Area = {area:.1f} mm2")
-        print(f"Ixx = {ixx_c:.3e} mm4")
-        print(f"Iyy = {iyy_c:.3e} mm4")
-        print(f"Ixy = {ixy_c:.3e} mm4")
-        print(f"Principal axis angle = {phi:.1f} deg")
-        print(f"Torsion constant = {j:.3e} mm4")
+        dictionaryProp[ilist] = {"A": area,
+                                 "xg": igx,
+                                 "yg": igy,
+                                 "Ixx": ixx_c,
+                                 "Iyy": iyy_c,
+                                 "Ixy": ixy_c,
+                                 "Wx+": wxx_pos,
+                                 "Wx-": wxx_neg,
+                                 "Wy+": wyy_pos,
+                                 "Wy-": wyy_neg,                                    
+                                 "c_torsion": j,
+                                 "Avx":  ax_c,
+                                 "Avy": ay_c,
+                                 
+                                }
+        
     
-    return listGeom, dictionaryProp
+    return dictGeom, dictionaryProp
+
+def calcTension(dictGeom, dataFile, unit = "KNm"):
+    if unit == "KNm":
+        c = 1000
+        
+    dictTension = {}
+        
+    for iName in dictGeom:
+        cds = dataFile.loc[dataFile['Nome']==iName]
+        dictTension[iName] = {}
+        for i in cds.values:
+            stress = dictGeom[iName].calculate_stress( n=i[2]*c, vx=i[3]*c, vy=i[4]*c, mzz=i[5]*c**2, mxx=i[6]*c**2, myy=i[7]*c**2 )
+            dictTension[iName][i[1]] = stress
+    
+    return dictTension
+
+def tensionVM_Max(dictTension):
+    tension = {}
+    for iN in dictTension:
+        tension[iN] = {}
+        for j in dictTension[iN]: #posso avere più combinazioni per sezione
+            tensionAll= dictTension[iN][j].get_stress()[0]
+            tension[iN][j] = max(abs(max(tensionAll["sig_vm"])), abs(min(tensionAll["sig_vm"])))
+    
+    return tension
+
+def _color_red_or_green(val):
+    color = 'red' if val > 1 else 'green'
+    #txt = f'color:clack;background-color:{color};'
+    txt = 'color: %s' % color
+    return txt
+
+def checkTension(tension, tLimit):
+    #fyd
+    df_DC =(pd.DataFrame(tension)/tLimit).round(2)
+    df_DC.style.applymap(_color_red_or_green)
+         
+    return df_DC
 
 """
 stress = sec.calculate_stress(
@@ -79,15 +139,17 @@ stress.plot_stress(stress="vm", cmap="viridis", normalize=False)
 StressPost.get_stress()
 """
 
-#path = r"C:\Users\Domen\OneDrive\00_TOLS\GitHub\SteelSectionCheck\example\Sezioni Pilone"
-#file = elenca_files_cartella(path, typeFile=".dxf")
-#listSection = importSection(file)
+# path = r"C:\Users\Domen\OneDrive\00_TOLS\GitHub\SteelSectionCheck\example\Sezioni Pilone"
+# file = elenca_files_cartella(path, typeFile=".dxf")
+# listSection = importSection(file, rotate=90)
+# #listSection["3C1"].plot_geometry()
+# geometryList, dictProp = calcPro(listSection)
+# dataFile = pd.read_excel(r'C:\Users\Domen\OneDrive\00_TOLS\GitHub\SteelSectionCheck\example\CDS_Pilone\CDS_pilone.xlsx')
+# tension = calcTension(geometryList, dataFile)
+# #tension["3C1"][1].plot_stress(stress="vm", cmap="YlOrRd", normalize=False, fmt="{x:.2f}")
+# #tension["3C1"][1].plot_stress(stress="myy_zz", cmap="YlOrRd", normalize=False, fmt="{x:.2f}")
+# #tension["3C1"][1].plot_stress(stress="mxx_zz", cmap="YlOrRd", normalize=False, fmt="{x:.2f}")
 
-#for i in listSection:
-    #Section(geometry=i).plot_mesh()
-    #section = Section(geometry=geom)
-    #section.calculate_geometric_properties()
-    #section.calculate_warping_properties()
-    #section.calculate_plastic_properties()
-
-    #section.plot_centroids()
+# #calcolo il massimo in valore assoluto
+# tMax = tensionVM_Max(tension)
+# checkTension(tMax, 355)
--- src/runApp.py
+++ src/runApp.py
@@ -5,12 +5,32 @@ from sectionproperties.pre import CompoundGeometry, Geometry, Material
 from tempfile import NamedTemporaryFile
 import os
 import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-# Folder picker button
-st.title('Make List of Files inside folder')
+st.set_page_config(layout="wide")
 
-st.write('Please insert path folder:')
+with st.sidebar:
+   st.title('section check')
+
+   # Utilizza st.markdown per inserire i link
+   st.markdown("## Contacts")
+   st.write("Name: Domenico")
+   st.write("Surname: Gaudioso")
+   st.write("📧 dome.gaudioso@gmail.com")
+   st.markdown("📱 [LinkedIn]({'https://www.linkedin.com/in/domenico-gaudioso-529a28171/'})", unsafe_allow_html=True)
+   st.markdown("💻 [GitHub]({'https://github.com/DomenicoGaudioso'})", unsafe_allow_html=True)
+
+   st.markdown("## About")
+   # Link di Streamlit
+   st.markdown(f"[Streamlit]({'https://www.streamlit.io/'})", unsafe_allow_html=True)
+   # Link di SciPy
+   st.markdown(f"[Sectionproperty]({'https://sectionproperties.readthedocs.io/en/latest/index.html'})", unsafe_allow_html=True)
+
+
+
+st.write('Please insert path folder conteinr dxf file section:')
 dirname = st.text_input('Path Folder', '\\')
 
 
@@ -18,15 +38,75 @@ if dirname is not "\\":
     dirname = os.path.normpath(dirname)
     file = elenca_files_cartella(dirname, typeFile=".dxf")
     st.write(file)
-
-    geometryList = importSection(file)
-
-## Assign material
-geometryList = assMat(geometryList , name = "Steel", E = 210e3, v=0.3, rho=7.85e-6, fy=250)
+    angle = st.number_input('angle rotated', value=90)
+    st.write('The current number is ', round(angle, 2))
+    geometryList = importSection(file, rotate=angle)
 
 ## Calculate property
-geometryList = calcPro(geometryList)
+geometryList, dictProp = calcPro(geometryList)
 
+## PLOT SECTION
+st.markdown("## Cross Section Geometry")
+plotList = []
+for i in geometryList:
+    fig, axs = plt.subplots(1, 2, figsize=(9, 3), sharey=True)
+    # plot the mesh
+    geometryList[i].plot_mesh(ax=axs[0])
+    geometryList[i].plot_centroids(ax=axs[1])
+    plotList.append(fig)
+    
+sectionPlot = st.slider("Which section do you want to see?",0, len(geometryList), 0)
+st.write(f'Section: {list(geometryList.keys())[sectionPlot]}')
+st.pyplot(plotList[sectionPlot])
 
+## PROPERTY SECTION
+st.markdown("## Property Setion")
+df = pd.DataFrame(dictProp)
+st.dataframe(df)
 
+st.write('Insert the cds excel file')
+uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    st.write(df)
+
+tension = calcTension(geometryList, df)
+
+#calcolo il massimo in valore assoluto
+tensionMax = tensionVM_Max(tension)
+st.markdown('## Tensione Max')
+df_tensMax =pd.DataFrame(tensionMax)
+st.dataframe(df_tensMax)
+
+#.map_index(lambda v: "color:pink;" if v>1 else "color:darkblue;", axis=0)
+st.markdown('## Rapporto Domanda/Capacità')
+sigmaLimit = st.number_input('tension limit', value=255/1.15)
+st.write('The current number is ', round(sigmaLimit, 2))
+check = checkTension(tensionMax, sigmaLimit)
+st.latex(r'\sqrt{(\sigma_{x,\text{Ed}})^2 + (\sigma_{z,\text{Ed}})^2 + \sigma_{z,\text{Ed}} \cdot \sigma_{x,\text{Ed}} + 3 \tau_{\text{Ed}}^2} \leq \sigma_{\text{yield}}')
+st.dataframe(check.style.applymap(lambda x: "background-color: red" if x>1 else "background-color: green"))
+
+## PLOT TENSION
+st.markdown("## Plot Tension")
+
+# plot the mesh
+cols1, cols2, cols3 = st.columns([1,1,1])
+
+nameSec = list(geometryList.keys())
+resultOpt = list(geometryList.keys())
+resultOpt    = ["vm", "n_zz", "mxx_zz","myy_zz",
+"m11_zz" ,"m22_zz","m_zz","mzz_zx","mzz_zy","mzz_zxy",
+"vx_zx","vx_zy", "vx_zxy","vy_zx","vy_zy","vy_zxy","v_zx","v_zy","v_zxy",
+"zz","zx","zy","zxy","11","33"]
+
+with cols1:
+    toggle1 = st.selectbox("section", nameSec)
+with cols2:
+    toggle2 =  st.selectbox("combination", list(tension[toggle1].keys()))
+with cols3:
+    toggle3 = st.selectbox( "tension plot", resultOpt, index=0)
+
+fig, axs = plt.subplots(1, 1, figsize=(9, 3), sharey=True)         
+tension[toggle1][toggle2].plot_stress(stress=toggle3, ax=axs, fmt="{x:.2f}")
+st.pyplot(fig)
 
